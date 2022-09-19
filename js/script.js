1a48fe6c0563f7cc83294a10cf5de1777a0e56ae
rav4353 <ravanthsri20@gmail.com>
2022-09-19 23:02:33 +0530

    Get-the-Value-of-Text-Input-Field-in-JavaScript

--- js/script.js
+++ js/script.js
@@ -1,20 +1,10 @@
-//
+var btn = document.getElementById("btn");
+var emptyArr = [];
 
-// let num = 7;
-// let i = num;
-// let emptyArr = [];
-//  {
-// emptyArr.push(i);
-
-// }
-// document.write(emptyArr);
-
-//
-
-function getInputValue() {
-
-    var inputVal = document.getElementById("myInput").value;
-
-
-    document.write(inputVal);
+function getVal() {
+   const val = document.getElementById('input').value;
+   let c = val.toString();
+   emptyArr.push(c);
+   document.getElementById("output").innerHTML = emptyArr;
 }
+btn.addEventListener("click", getVal);
